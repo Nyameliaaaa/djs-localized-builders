940541df193ba9a9cccfa38642f50bccb32b2306
Amelia Katherine <32780515+night-lake@users.noreply.github.com>
2023-02-08 23:35:50 +0300

    docs: update example

--- README.md
+++ README.md
@@ -10,10 +10,22 @@ Install `djs-localized-builders` using your favorite package manager and then ad
 // initalize i18n here
 
 setConfig({
-    stuff: 'yeah'
+    getLocalizedString: ({ namespace, string, lang, options }) => {
+        return this.i18n.getString({ namespace, string, lang, options }) ?? 'fetch_fail';
+    },
+    useUppercaseConversionValues: false,
+    seperatorChar: '.',
+    validators: proccess.ENV.NODE_ENV === 'production',
+    langs: ['en-US'],
+    namespaces: {
+        components: 'responses',
+        commands: 'commands',
+        embeds: 'responses'
+    }
 });
 
 // load commands here
 ```
 
-⚠️ **IMPORTANT**: You MUST set the configuration for the library AFTER your i18n library is initialized but BEFORE your commands are loaded in order to ensure the builders can properly recieve strings.
+> **Warning**
+> You MUST set the configuration for the library AFTER your i18n library is initialized but BEFORE your commands are loaded in order to ensure the builders can properly recieve strings.
--- src/config.ts
+++ src/config.ts
@@ -1,95 +1,5 @@
 import { disableValidators, enableValidators } from '@discordjs/builders';
-
-/**
- * The names to use for the different namespaces from where strings are fetched.
- * @category Config
- */
-export interface NamespaceMap {
-    /**
-     * The namespace used to fetch command localization strings.
-     * @defaultValue 'commands'
-     */
-    commands: string;
-
-    /**
-     * The namespace used to fetch component localization strings.
-     * @defaultValue 'components'
-     */
-    components: string;
-
-    /**
-     * The namespace used to fetch embed localization strings.
-     * @defaultValue 'embeds'
-     */
-    embeds: string;
-}
-
-/**
- * The options for the `options.getLocalizedString`
- * @category Config
- */
-export interface GetLocalizedStringOptions {
-    /**
-     * The namespace from which the string is fetched.
-     */
-    namespace: string;
-
-    /**
-     * The string in question
-     */
-    string: string;
-
-    /**
-     * The locale to fetch from.
-     */
-    lang: string;
-
-    /**
-     * Any additional Key/Value arguments for the string.
-     */
-    options?: Record<string, any>;
-}
-
-/**
- * The type for the library config.
- * @category Config
- */
-export interface ConfigType {
-    /**
-     * The function which is responsible for fetching a localized string. You must define this for the library to function.
-     * @param {GetLocalizedStringOptions} options - The options passed by the function.
-     */
-    getLocalizedString: (options: GetLocalizedStringOptions) => string;
-
-    /**
-     * Whether to uppercase the values of Select Menu Options & Option Choices when fetching the string.
-     * @defaultValue `false`
-     */
-    useUppercaseConversionForValues: boolean;
-
-    /**
-     * The char to join the base keys with.
-     * @defaultValue '.'
-     */
-    seperatorChar: string;
-
-    /**
-     * Modify the default namespace names used by the library for string fetching.
-     */
-    namespaces?: NamespaceMap;
-
-    /**
-     * The languages that the builders should support.
-     * @defaultValue 'en-US'
-     */
-    langs: string[];
-
-    /**
-     * Whether to enable the Discord.js Builder validators.
-     * @defaultValue `true`
-     */
-    validators?: boolean;
-}
+import { ConfigType } from 'types';
 
 let config: ConfigType = {
     getLocalizedString: ({ string }) => `function_not_implemented_${string.toLocaleLowerCase()}`,
@@ -113,7 +23,8 @@ let config: ConfigType = {
 		},
 		useUppercaseConversionValues: false,
 		seperatorChar: '_',
-		validators: proccess.ENV.NODE_ENV === 'development',
+		validators: proccess.ENV.NODE_ENV === 'production',
+        langs: ['en-US'],
 		namespaces: {
 			components: 'responses',
 			commands: 'commands',
