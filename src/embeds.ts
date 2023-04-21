fdd21818eac7ec54b2e556a67f426cd10a8e73f4
Amelia Katherine <32780515+night-lake@users.noreply.github.com>
2023-04-21 17:24:21 +0300

    fix(embed): export class

--- src/embeds.ts
+++ src/embeds.ts
@@ -2,10 +2,10 @@ import { LocaleBaseKeyMixin, BuilderMixin } from 'mixins';
 import { EmbedBuilder as Builder } from '@discordjs/builders';
 import { mix } from 'ts-mixer';
 
-interface EmbedBuilder extends BuilderMixin<Builder>, LocaleBaseKeyMixin {}
+export interface EmbedBuilder extends BuilderMixin<Builder>, LocaleBaseKeyMixin {}
 
 @mix(LocaleBaseKeyMixin, BuilderMixin)
-class EmbedBuilder {
+export class EmbedBuilder {
     constructor(locale: string, baseKey?: string) {
         this.locale = locale;
         this.baseKey = baseKey;
